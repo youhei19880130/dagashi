9e30662ec56ab9120a5f9adae6ec396b614d69c7
yohhei someya <youhei19880130@yahoo.co.jp>
2015-11-24 03:48:53 +0900

    [add] refine_detail_page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,7 +50,19 @@ ready = function(){
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  $('.uiGrid ._51mz .pluginConnectButtonLayoutRoot ._3c9t').css('width','auto');
+  // Google map表示
+  // キャンパスの要素を取得する
+  var canvas = document.getElementById( 'map-canvas' ) ;
+  // 中心の位置座標を指定する
+  var latlng = new google.maps.LatLng(35.6610775, 139.6994961);
+  // 地図のオプションを設定する
+  var mapOptions = {
+    zoom: 16,        // ズーム値
+    center: latlng,    // 中心座標 [latlng]
+  };
+
+  // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+  var map = new google.maps.Map( canvas , mapOptions ) ;
 };
 
 $(document).ready(ready)
